5ebec64bd2233ab801dd8314d449fb406f319f63
Tanner Chartier <chartiert@netsysplus.com>
2024-09-23 12:32:12 -0500

    09_23_2024_12_32

--- PushToPublic.ps1
+++ PushToPublic.ps1
@@ -3,8 +3,8 @@
 if ($psISE) {$BaseDir = Split-Path -Path $psISE.CurrentFile.FullPath    #IF running in ISE, with line by line execution this will work
 } else {$BaseDir = $PSScriptRoot}
 
-$RepoPath = "$BaseDir"
-set-location "$($BaseDir)"
+$RepoPath = "C:\ASN_GitRepos\Public"
+set-location "$($RepoPath)"
 
 git add --all
 git commit -m "$(get-date -f `"MM_dd_yyyy_HH_mm`")"
